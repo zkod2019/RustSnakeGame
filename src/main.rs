4497d7735d214b1618a5e4c602ccae61dc3100c3
Zaida Kodzoeva <zkod777@gmail.com>
2023-04-15 02:47:00 +0400

    update: made background + snake head

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use opengl_graphics::{GlGraphics, OpenGL};
 
 struct Game{
     gl: GlGraphics,
+    snake: Snake,
 }
 
 impl Game {
@@ -22,10 +23,30 @@ impl Game {
         self.gl.draw(arg.viewport(), |c, gl|{
             graphics::clear(PINK, gl);
         });
-
+        self.snake.render(&mut self.gl, arg);
     }
 }
 
+struct Snake{
+    x_position: i32,
+    y_position: i32,
+}
+
+impl Snake{
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs){
+        use graphics;
+
+        let BLACK: [f32; 4]= [0.0, 0.0, 0.0, 1.0];
+
+        let square = graphics::rectangle::square(self.x_position
+            as f64, self.y_position as f64, 20_f64);
+
+        gl.draw(args.viewport(), |c, gl|{
+            let transform = c.transform;
+            graphics::rectangle(BLACK, square, transform, gl);
+        });
+    }
+}
 
 fn main() {
     let opengl = OpenGL::V3_2; // open window
@@ -35,7 +56,8 @@ fn main() {
     ).opengl(opengl).exit_on_esc(true).build().unwrap();
 
     let mut game = Game{
-        gl: GlGraphics::new(opengl)
+        gl: GlGraphics::new(opengl),
+        snake: Snake{x_position:50, y_position:100}, // initializing snake
     };
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window){
